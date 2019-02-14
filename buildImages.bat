051e17ebffd9897b1bd7738bb88ff585f85de72f
Younes <younesberrada1@gmail.com>
2019-02-14 17:40:48 +0100

    update batch file

--- buildImages.bat
+++ buildImages.bat
@@ -1,21 +1,21 @@
 @echo on
 :: THIS BUILDS DOCKER IMAGES
 cd admin
-docker build --tag registry.ng.bluemix.net/la_factory/admin-image.
+docker build --tag registry.ng.bluemix.net/la_factory/admin-image .
 cd ..
 
 cd authentification
-docker build --tag registry.ng.bluemix.net/la_factory/auth-image.
+docker build --tag registry.ng.bluemix.net/la_factory/auth-image .
 cd ..
 
 cd bill
-docker build --tag registry.ng.bluemix.net/la_factory/bill-image.
+docker build --tag registry.ng.bluemix.net/la_factory/bill-image .
 cd ..
 
 cd chatbot
-docker build --tag registry.ng.bluemix.net/la_factory/chat-image.
+docker build --tag registry.ng.bluemix.net/la_factory/chat-image .
 cd ..
 
 cd server
-docker build --tag registry.ng.bluemix.net/la_factory/server-image.
+docker build --tag registry.ng.bluemix.net/la_factory/server-image .
 cd ..
